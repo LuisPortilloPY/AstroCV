12f8269b1efd36532cbf4762884ddf817db82ad5
Luis Portillo <portillov.luis@gmail.com>
2025-01-05 11:43:44 -0300

    This project was based on the project done by Gianmarco
    The original repository is https://github.com/Ladvace

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const LINKS = {
+  github: "https://github.com/LuisPortilloPY",
+  linkedin: "https://www.linkedin.com/in/kikeportillo/",
+  discord: "https://discordapp.com/users/kikextillo",
+  email: "portillov.luis@gmail.com<",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
